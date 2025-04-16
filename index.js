9c3582b59bfa82232cd34c305c61a148987a6e6f
RottenSonGames <sanghoyou@gmail.com>
2025-04-16 16:09:31 +0800

    validation added

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-console.log("MONGODB_URI IS:", process.env.MONGODB_URI);
 
 const express = require('express')
 const app = express()
@@ -7,101 +6,74 @@ app.use(express.json())
 app.use(express.static('dist'))
 
 const morgan = require('morgan')
-// app.use(morgan('tiny'))
-morgan.token('body', (req) => JSON.stringify(req.body))
+morgan.token ('body', (request) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :response-time ms :body'))
 
 const Contact = require('./models/contact')
 
 app.get('/api/contacts', (request, response, next) => {
-    Contact.find({}).then(contacts => response.json(contacts))
-                    .catch(error => next(error))
+  Contact.find({})
+    .then(contacts => response.json(contacts))
+    .catch(error => next(error))
 })
 
 app.get('/api/contacts/:id', (request, response, next) => {
-    const id = request.params.id
-    Contact.findById(id)
-        .then(note => {
-            if (note) response.json(contact)
-            else response.status(404).end()
-        })
-        .catch(error => next(error))
+  Contact.findById(request.params.id)
+    .then(contact => {
+      if (contact) response.json(contact)
+      else response.status(404).end()
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/contacts/info', (request, response, next) => {
   Contact.find({})
     .then(contacts => {
-        response.send(`<p>Phonebook has info for ${contacts.length} people</p>
-                       <p>${new Date()}</p>`)
+      response.send(`<p>Phonebook has ${contacts.length} contacts</p>
+                    <p>${new Date()}</p>`)
     })
     .catch(error => next(error))
-
 })
 
-app.post('/api/contacts/', (request, response) => {
-    const body = request.body
-    if (!body.name || !body.number) {
-      return response.status(400)
-                     .json({error: 'new entry missing name and/or number'})
-    }
-    // else if (contacts.some(contact => contact.name === body.name)) {
-    //   return response.status(400).json({error: 'name already exists'})
-    // }
-    const contact = new Contact({
-        name: body.name,
-        number: body.number
-    })
-    contact.save()
-           .then(savedContact => response.json(savedContact))
+app.post('/api/contacts', (request, response, next) => {
+  const contact = new Contact({
+    name: request.body.name,
+    number: request.body.number
+  })
+  contact.save()
+    .then(savedContact => response.json(savedContact))
+    .catch(error => next(error))
 })
 
 app.put('/api/contacts/:id', (request, response, next) => {
-    const id = request.params.id
-    const body = request.body
-    if (!body.name || !body.number) {
-      return response.status(400)
-                     .json({error: 'new entry missing name and/or number'})
-    }
-    Contact.findById(id)
-        .then(contact => {
-            if (!contact) {
-                return response.status(404).end()
-            }
-
-            contact.name = body.name;
-            contact.number = body.number;
-
-            return contact.save().then((updatedContact) => {
-                response.json(updatedContact)
-            })
-        })
-        .catch(error => next(error))
+  Contact.findByIdAndUpdate(request.params.id)
+    .then(contact => {
+      if (!contact) return response.status(404).end()
+      contact.name = request.body.name
+      contact.number = request.body.number
+      return contact.save().then((updatedContact) => response.json(updatedContact))
+    })  
+    .catch(error => next(error))
 })
 
 app.delete('/api/contacts/:id', (request, response, next) => {
-    const id = request.params.id
-    Contact.findByIdAndDelete(id)
-        .then(result => response.status(204).end())
-        .catch(error => next(error))
+  Contact.findByIdAndDelete(request.params.id)
+    .then(result => response.status(204).end())
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'})
+    return response.status(404).send({error: 'unknown endpoint'})
 }
-app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+    console.log(error)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({error: 'malformatted id'})
-    }
+    if (error.name === 'CastError') return response.status(400).send({error: 'malformatted id'})
+    else if (error.name === 'ValidationError') return response.status(400).send({error: 'name or number missing'})
 
     next(error)
 }
-app.use(errorHandler)
 
 const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT)
--- models/contact.js
+++ models/contact.js
@@ -2,22 +2,31 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
-console.log("attempting to connect to", url)
 mongoose.connect(url)
-  .then(result => console.log("connection to MongoDB successful"))
-  .catch(error => console.log("error connecting to MongoDB: ", error.message))
+  .then(result => console.log('successfully connected to MongoDB'))
+  .catch(error => console.log('error connecting to MongoDB: ', error.message))
 
 const contactSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true
+  },
 })
 
 contactSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Contact', contactSchema)
+
+
